1ef8409a8371d18fce01f3b9a07d282ed28356c0
Victor Gabardini <vicgaba@gmail.com>
2025-07-26 20:27:06 -0300

    First commit

---
+++ src/main/java/ar/com/sistemasnea/diexample/DiexampleApplication.java
@@ -0,0 +1,13 @@
+package ar.com.sistemasnea.diexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiexampleApplication.class, args);
+	}
+
+}
